f40a6a538174386ffb391875612432026d86c35c
Jay0137 <bryanjayson2017@gmail.com>
2024-02-28 22:27:52 -0400

    2/28/2024
    Added new logo

--- z/src/_auth/forms/SigninForm.tsx
+++ z/src/_auth/forms/SigninForm.tsx
@@ -44,7 +44,7 @@ const SigninForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-      <img src="/assets/images/logo.png" alt="logo" />
+      <img src="/assets/images/logo-bg-0.png" alt="logo" />
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
           Sign in into your account</h2>
