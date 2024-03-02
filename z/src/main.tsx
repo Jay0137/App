0ddf2e06a0e0cd14c4c3d012e1940c6e2264776f
Jay0137 <bryanjayson2017@gmail.com>
2024-03-01 22:30:20 -0400

    3/1/2024
    added for to main page to make buttons for log in and sign in pages

--- z/src/main.tsx
+++ z/src/main.tsx
@@ -9,4 +9,38 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </BrowserRouter>
 
     // make to route sign in or sign up pages
+
 )  
+
+
+/////////////////////////////////////////////////////////////
+
+<Form>
+    <form>
+        <p className="text-small-regular text-black text-center mt-2">
+        Dont have a account? 
+        <Link to="/sign-up" className="text-primary-500" text-small-semibold ml-1> Sign up</Link>
+        </p>
+    </form>
+    
+    <form>
+        <p className="text-small-regular text-black text-center mt-2">
+        Already have a account? 
+        <Link to="/sign-in" className="text-primary-500" text-small-semibold ml-1> Sign in</Link>
+        </p>
+    </form>
+
+</Form>
+ // fuse link and button 
+ // make 2 bottom 1 for sign in and 1 for sign up
+ // color 1button - white, 2botton - blue
+ //
+<Button type="submit" className="shad-button_primary">
+{isLoading ? (
+  <div className="flex-center gap-2">
+    <Loader /> Loading...
+  </div>
+):"Sign up"}
+</Button>
+
+//////////////////////////////////////////////////////////////////////
